3581b00ef1c018d5483e6a5f9e169596de9d1ea4
Gustavo Ferreira <65983794+gustavoofdeooliveira@users.noreply.github.com>
2020-06-15 09:58:37 -0300

    fim do exercico 4

--- atividade4.php
+++ atividade4.php
@@ -1,4 +1,5 @@
 <?php
+//Atividade 1)A)
 $x = "10";
 $y = 10;
 echo ($x == $y)."<br>";
@@ -10,3 +11,40 @@ echo ($x !== $y);
             
              1 */ 
 ?><br><br>
+
+<?php
+//B
+$x = 15;
+$y = 10;
+echo ($x > $y)."<br>";
+echo ($x < $y)."<br>";
+echo ($x >= $y)."<br>";
+echo ($x <= $y);
+/*Resultado: 
+            1
+            
+            1*/
+?><br><br>
+
+<?php
+//C
+$a = true;
+$b = false;
+echo ($a && $b)."<br>";
+echo ($a || $b)."<br>";
+echo (!$a);
+/*Resultado: 
+
+            1*/
+?><br><br>
+
+<?php
+//D
+$a = "Oie";
+echo ($a && $b)."<br>";
+echo ($a || $b)."<br>";
+echo (!$a);
+/*Resultado: 
+
+            1*/
+?>
