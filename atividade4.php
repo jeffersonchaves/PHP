23003c86636b7af73f8c289284fa9004be521283
Gustavo Ferreira <65983794+gustavoofdeooliveira@users.noreply.github.com>
2020-06-15 09:48:54 -0300

    Create atividade4.php

---
+++ atividade4.php
@@ -0,0 +1,12 @@
+<?php
+$x = "10";
+$y = 10;
+echo ($x == $y)."<br>";
+echo ($x ==+ $y)."<br>";
+echo ($x != $y)."<br>";
+echo ($x !== $y);
+/*Resultado: 1
+             1
+            
+             1 */ 
+?><br><br>
