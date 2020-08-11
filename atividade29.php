fc06ef8a32c6d9226d369afb485a34db51af81c2
Gustavo Ferreira <65983794+gustavoofdeooliveira@users.noreply.github.com>
2020-08-11 14:30:35 -0300

    atualização

--- atividade29.php
+++ atividade29.php
@@ -1,7 +1,10 @@
 <?php
-    $gerado1 = rand(1,10);
-    $gerado2 = rand(10,20);
-    for($a = $gerado1; $a<=$gerado2; $a++){
-        echo $a." ";
+    $gerado1 = rand(1,16);
+    if($gerado1 != 0 && 2 && 5 && 13 && 10 && 16 && 6 && 7 && 1 && 14 && 12 && 9 && 15 && 8 && 11 && 3){
+        $gerado1 = rand(1,16);
+        echo $gerado1; 
+
+    }else{
+   echo $gerado1;
     }
 ?>
