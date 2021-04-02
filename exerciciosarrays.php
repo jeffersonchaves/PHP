601ee14cc8de76d2a40454c3e943adcb54448650
Gustavo Ferreira <65983794+gustavoofdeooliveira@users.noreply.github.com>
2021-04-02 15:48:12 -0300

    Update exerciciosarrays.php

--- exerciciosarrays.php
+++ exerciciosarrays.php
@@ -1,11 +1,8 @@
 <?php
 $numero = array();
 
-    for ($i = 0; $i <=4 ; $i++) {
-        print "digite um número: ";
-        $numero[$i] = (int) fgets(STDIN);
-
+for ($i = 0; $i <= 4; $i++) {
+    print "digite um número: ";
+    $numero[$i] = (int) fgets(STDIN);
 }
 print_r($numero);
-
-?>
