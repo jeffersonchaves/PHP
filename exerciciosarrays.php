d8b7dd45327888516c38baa04b13e48c6df00505
Gustavo Ferreira <65983794+gustavoofdeooliveira@users.noreply.github.com>
2021-04-02 15:15:49 -0300

    inicio dos exercicios

--- exerciciosarrays.php
+++ exerciciosarrays.php
@@ -1,4 +1,14 @@
 <?php
-// Obtém três comandos do usuário
+$numero = array();
+
+    for ($i = 0; $i <=4 ; $i++) {
+        print "digite um número: ";
+$numero = (int) fgets(STDIN);
+        echo $numero[$i];
+}
+for ($i = 0; $i <=4 ; $i++) {
+    echo $numero[$i];
+}
+
 
 ?>
