90020b3b28b66bd3a61a0f1c0e2a610c43684c7d
Gustavo Ferreira <65983794+gustavoofdeooliveira@users.noreply.github.com>
2021-04-08 09:31:53 -0300

    exercicios prontos

--- exerciciosarrays7.php
+++ exerciciosarrays7.php
@@ -1,5 +1,25 @@
-<?php
-echo "Qual é o seu nome de viajante: ";
-Echo "O mês você nasceu";
-$vetorMes[] = "1 - Janeiro, 2 - Fevereiro, 3 - Marco,4 - Abril, 5 - Maio, 6 - Junho, 7 - Julho, 8 - Agosto,9 - Setembro, 10 - Outubro, 11 - Novembro, 12 - Dezembro";
-$numero[$mes] = (int) fgets(STDIN);
+<!DOCTYPE html>
+
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Qual é o seu nome de viajante</title>
+</head>
+
+<body style="background-color:aqua; text-align:center;">
+    <h1 style="margin-top:15%; font-size: 40px; color:yellow">
+Qual é o seu nome <div style="color:orange; font-weight:bold;">de viajante?</div>
+            
+    </h1>
+    <h3 style="color: #fff;"><?php
+        $vetorMes = array("Planejador(a) de roteiro", "Farofeiro(a)", "Viajante", "Louco das promoções de passagens", " Hipster", "Aventureiro(a)", "Turistão/Turistona", "Fotógrafo(a)", "Mochileiro(a)", "Amigão/Amigona", "Explorador(a)", "Consumista");
+        $vetorAniversario = array("que se perde toda vez", "que controla os gastos da galera", "da excursão da terceira idade", "da quebrada", "que leva açai", "do rolê", "que ama souvenir", "gourmet", "que não quer voltar para casa", "dos destinos nacionais", "que senta na janelinha", "do hotel cinco estrelas");
+        $numeroMes = rand(0, 11);
+        $numeroAniversario = rand(0, 11);
+
+        echo "$vetorMes[$numeroMes], $vetorAniversario[$numeroAniversario]";
+        ?></h3>
+</body>
+
+</html>
